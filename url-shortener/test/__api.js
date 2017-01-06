a56c8094dd93ded6e51911743f2b60e36e5d4668
Danielle Monroe <dmonroe08@fullsail.edu>
2017-01-05 21:51:25 -0500

    places files in correct folder

--- url-shortener/test/__api.js
+++ url-shortener/test/__api.js
@@ -1,20 +1,16 @@
 var server = require('supertest');
-describe('API', function() {
-  var server;
-  beforeEach(function(){
-    server = require('../src/server.js');
-  });
-  afterEach(function(){
-    server.close();
-  });
-  it('/Please Work', function testapi(done) {
+
+describe('api', function() {
+  server = require('supertest');
+  
+  it('/ status', function testapi(done) {
     request(server)
     .get('/api/')
     .set('Accept', 'application/json')
     .expect('Content-Type' , /json/)
     .expect(200, done);
 });
-    it('/Pretty Please Work', function testapi(done) {
+    it('/ string Please Work', function testapi(done) {
       request(server)
       .get('/api/string')
       .set('Accept', 'application/json')
